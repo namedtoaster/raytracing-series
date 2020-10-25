f07a6f213deace2e6586b71edd295befea42662d
Dave Campbell <djcampbell1914@gmail.com>
2020-10-25 13:43:19 -0400

    Add sphere hit function

--- OneWeekend/source/main.cpp
+++ OneWeekend/source/main.cpp
@@ -1,14 +1,9 @@
 #include "vec3.h"
 #include "ray.h"
 #include "color.h"
+#include "utilities.h"
 
 #include <iostream>
-
-color ray_color(const ray& r) {
-    vec3 unit_direction = unit_vector(r.direction());
-    auto t = 0.5*(unit_direction.y() + 1.0);
-    return (1.0-t)*color(1.0, 1.0, 1.0) + t*color(0.5, 0.7, 1.0);
-}
 
 int main() {
 
